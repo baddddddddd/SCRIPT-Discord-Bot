ad48063dfa6b19aaa5caccc0c817f0e4e1630181
baddddddddd <eluxeofficial@gmail.com>
2024-11-10 08:28:35 +0800

    Add logging for runner.js

--- runner.js
+++ runner.js
@@ -1,6 +1,7 @@
 import { spawn } from 'child_process';
 
 function startBot() {
+  console.log("Starting process...")
   const botProcess = spawn('node', ['index.js'], { stdio: 'inherit' });
 
   botProcess.on('close', (code) => {
